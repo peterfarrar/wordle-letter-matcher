34773e4ee76bb2e35892990b071d24c34e4aea4e
Peter Farrar <seven@sevenhuman.net>
2022-06-01 14:05:08 -0400

    Reorganized and expanded wordmatch tests

--- test/wordmatch.spec.js
+++ test/wordmatch.spec.js
@@ -2,68 +2,150 @@ const { expect } = require('chai')
 const { checkTheGuess } = require('../src/wordmatch')
 
 describe('match a guess', () => {
-  it('has the letter once in the wrong position', () => {
-    const word = 'teams'
-    const guess = 'enjoy'
+  context('exists zero times', () => {
+    it('guessed a no letters that exist in the answer', () => {
+      const word = 'birds'
+      const guess ='campy'
 
-    expect(checkTheGuess(word, guess)).to.eql([
-      { 'e': 'wrong-position' },
-      { 'n': 'invalid-letter' },
-      { 'j': 'invalid-letter' },
-      { 'o': 'invalid-letter' },
-      { 'y': 'invalid-letter' },
-    ])
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
+        { 'c': 'invalid-letter' },
+        { 'a': 'invalid-letter' },
+        { 'm': 'invalid-letter' },
+        { 'p': 'invalid-letter' },
+        { 'y': 'invalid-letter' },
+      ])
+    })
   })
+
+  context('exists one time', () => {
+    it('guessed one letter in the wrong position', () => {
+      const word = 'teams'
+      const guess = 'enjoy'
   
-  it('has the letter once in the right position', () => {
-    const word = 'teams'
-    const guess = 'jelly'
 
-    expect(checkTheGuess(word, guess)).to.eql([
-      { 'j': 'invalid-letter' },
-      { 'e': 'correct-position' },
-      { 'l': 'invalid-letter' },
-      { 'l': 'invalid-letter' },
-      { 'y': 'invalid-letter' },
-    ])
-  })
-  
-  it('has the letter once, but guessed twice in the wrong position', () => {
-    const word = 'teams'
-    const guess = 'breez'
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
+        { 'e': 'wrong-position' },
+        { 'n': 'invalid-letter' },
+        { 'j': 'invalid-letter' },
+        { 'o': 'invalid-letter' },
+        { 'y': 'invalid-letter' },
+      ])
+    })
 
-    expect(checkTheGuess(word, guess)).to.eql([
+    it('guessed one letter in the right position', () => {
+      const word = 'teams'
+      const guess = 'jelly'
+
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
+        { 'j': 'invalid-letter' },
+        { 'e': 'correct-position' },
+        { 'l': 'invalid-letter' },
+        { 'l': 'invalid-letter' },
+        { 'y': 'invalid-letter' },
+      ])
+    })
+
+    it('guessed a the letter that exists once, twice in the wrong position', () => {
+      const word = 'teams'
+      const guess = 'breez'
+
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
+        { 'b': 'invalid-letter' },
+        { 'r': 'invalid-letter' },
+        { 'e': 'wrong-position' },
+        { 'e': 'invalid-letter' },
+        { 'z': 'invalid-letter' },
+      ])
+    })
+
+    it('guessed a letter that exists once, twice, first in the right position', () => {
+      const word = 'teams'
+      const guess = 'beely'
+
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
       { 'b': 'invalid-letter' },
-      { 'r': 'invalid-letter' },
-      { 'e': 'wrong-position' },
-      { 'e': 'invalid-letter' },
-      { 'z': 'invalid-letter' },
-    ])
+        { 'e': 'correct-position' },
+        { 'e': 'invalid-letter' },
+        { 'l': 'invalid-letter' },
+        { 'y': 'invalid-letter' },
+      ])
+    })
+
+    it('guessed a letter that exists once, twice, first in the wrong position, second in the right position', () => {
+      const word = 'teams'
+      const guess = 'beely'
+
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
+        { 'b': 'invalid-letter' },
+        { 'e': 'correct-position' },
+        { 'e': 'invalid-letter' },
+        { 'l': 'invalid-letter' },
+        { 'y': 'invalid-letter' },
+      ])
+    })
   })
 
-  it('has the letter once, but guessed twice, once in the right position', () => {
-    const word = 'teams'
-    const guess = 'beely'
+  context('exists two time', () => {
+    it('guessed the letter twice, both in the wrong position', () => {
+      const word = 'speed'
+      const guess = 'elude'
 
-    expect(checkTheGuess(word, guess)).to.eql([
-      { 'b': 'invalid-letter' },
-      { 'e': 'correct-position' },
-      { 'e': 'invalid-letter' },
-      { 'l': 'invalid-letter' },
-      { 'y': 'invalid-letter' },
-    ])
-  })
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
+        { 'e': 'wrong-position' },
+        { 'l': 'invalid-letter' },
+        { 'u': 'invalid-letter' },
+        { 'd': 'wrong-position' },
+        { 'e': 'wrong-position' },
+      ])
+    })
 
-  it('has the letter once, but guessed twice, first in the wrong position, second in the right position', () => {
-    const word = 'teams'
-    const guess = 'beely'
+    it('guessed the letter twice, first guess in the right position', () => {
+      const word = 'speed'
+      const guess = 'breme'
 
-    expect(checkTheGuess(word, guess)).to.eql([
-      { 'b': 'invalid-letter' },
-      { 'e': 'correct-position' },
-      { 'e': 'invalid-letter' },
-      { 'l': 'invalid-letter' },
-      { 'y': 'invalid-letter' },
-    ])
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
+        { 'b': 'invalid-letter' },
+        { 'r': 'invalid-letter' },
+        { 'e': 'correct-position' },
+        { 'm': 'invalid-letter' },
+        { 'e': 'wrong-position' },
+      ])
+    })
+
+    it('guessed the letter twice, second guess in the right position', () => {
+      const word = 'speed'
+      const guess = 'elder'
+
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
+        { 'e': 'wrong-position' },
+        { 'l': 'invalid-letter' },
+        { 'd': 'wrong-position' },
+        { 'e': 'correct-position' },
+        { 'r': 'invalid-letter' },
+      ])
+    })
+
+    it('guessed the letter twice, both guesses in the right position', () => {
+      const word = 'speed'
+      const guess = 'steel'
+
+      const actual = checkTheGuess(word, guess)
+      expect(actual).to.eql([
+        { 's': 'correct-position' },
+        { 't': 'invalid-letter' },
+        { 'e': 'correct-position' },
+        { 'e': 'correct-position' },
+        { 'l': 'invalid-letter' },
+      ])
+    })
   })
 })
